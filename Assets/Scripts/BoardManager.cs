749d96f064e882b6f194e13c064206e8a1455ab6
Haftan666 <j.haftan666@gmail.com>
2024-11-07 23:08:44 +0100

    Code Refactor
    Moved certain things to other files

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -7,7 +7,6 @@ public enum Team
     WHITE,
     BLACK
 }
-
 public class BoardManager : MonoBehaviour
 {
     public Piece[,] pieces = new Piece[8, 8];
@@ -16,8 +15,8 @@ public class BoardManager : MonoBehaviour
     public Vector3 boardOffset = new Vector3(-3.507f, 0.3571f, -3.485f);
     public Vector3 pieceOffset = new Vector3(3.23f - 3.507f, 0, 3.37f - 3.485f);
 
-    private GameObject whitePiecesParent;
-    private GameObject blackPiecesParent;
+    public GameObject whitePiecesParent { get; set; }
+    public GameObject blackPiecesParent { get; set; }
 
     private Vector2 mouseOver;
     private Vector2 startDrag;
@@ -25,10 +24,27 @@ public class BoardManager : MonoBehaviour
     private Piece selectedPiece;
     private bool isWhiteTurn = true;
 
+    public Piece SelectedPiece
+    {
+        get { return selectedPiece; }
+        set { selectedPiece = value; }
+    }
+
+    public Vector2 StartDrag
+    {
+        get { return startDrag; }
+        set { startDrag = value; }
+    }
+
+    public bool IsWhiteTurn
+    {
+        get { return isWhiteTurn; }
+        set { isWhiteTurn = value; }
+    }
 
     private void Start()
     {
-        GenerateBoard();
+        BoardGenerator.GenerateBoard(this);
     }
 
     private void Update()
@@ -45,14 +61,14 @@ public class BoardManager : MonoBehaviour
 
         if (selectedPiece != null)
         {
-            DragPiece(selectedPiece);
+            PieceMovement.DragPiece(selectedPiece);
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             if (selectedPiece != null)
             {
-                TryMove((int)startDrag.x, (int)startDrag.y, x, y, selectedPiece.rank);
+                PieceMovement.TryMove(this, (int)startDrag.x, (int)startDrag.y, x, y, selectedPiece.rank);
                 selectedPiece = null;
             }
         }
@@ -82,7 +98,7 @@ public class BoardManager : MonoBehaviour
 
     private void SelectPiece(int x, int y)
     {
-        if (x < 0 || x >= pieces.Length || y < 0 || y >= pieces.Length)
+        if (x < 0 || x >= pieces.GetLength(0) || y < 0 || y >= pieces.GetLength(1))
             return;
 
         Piece p = pieces[x, y];
@@ -94,330 +110,9 @@ public class BoardManager : MonoBehaviour
         }
     }
 
-    private void DragPiece(Piece piece)
+    public void EndTurn()
     {
-        if (!Camera.main)
-        {
-            Debug.Log("Unable to find main camera");
-            return;
-        }
-
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
-        {
-            piece.transform.position = hit.point + Vector3.up * 0.5f;
-        }
+        isWhiteTurn = !isWhiteTurn;
+        StartCoroutine(CameraController.RotateCamera(1.0f, isWhiteTurn));
     }
-
-    private void TryMove(int startX, int startY, int endX, int endY, Rank rank)
-    {
-        if (IsValidMove(startX, startY, endX, endY, rank))
-        {
-            Piece piece = pieces[startX, startY];
-            pieces[endX, endY] = piece;
-            pieces[startX, startY] = null;
-            piece.transform.position = new Vector3(endX - 0.277f, 0, endY - 0.115f);
-
-            PromotePieceIfNeeded(piece, endY);
-
-            bool captured = false;
-            if (rank == Rank.Knight)
-            {
-                captured = CapturePawnsPassedByKnight(startX, startY, endX, endY);
-            }
-            else
-            {
-                captured = CapturePawnIfNeeded(startX, startY, endX, endY);
-            }
-
-            // Check if the piece can continue capturing
-            if (captured && CanCapture(endX, endY, piece.rank))
-            {
-                selectedPiece = piece;
-                startDrag = new Vector2(endX, endY);
-                return; // Do not end the turn
-            }
-
-            isWhiteTurn = !isWhiteTurn;
-            StartCoroutine(RotateCamera(1.0f)); // Rotate over 1 second
-        }
-        else
-        {
-            // Invalid move, reset the piece position
-            selectedPiece.transform.position = new Vector3(startX - 0.277f, 0, startY - 0.115f);
-        }
-    }
-
-    private void PromotePieceIfNeeded(Piece piece, int endY)
-    {
-        if (piece.team == Team.WHITE && endY == 7)
-        {
-            piece.transform.rotation = Quaternion.Euler(90, 0, 0);
-            piece.rank = Rank.Knight;
-        }
-        else if (piece.team == Team.BLACK && endY == 0)
-        {
-            piece.transform.rotation = Quaternion.Euler(90, 0, 180);
-            piece.rank = Rank.Knight;
-        }
-    }
-
-    private bool CapturePawnsPassedByKnight(int startX, int startY, int endX, int endY)
-    {
-        bool captured = false;
-        int stepX = (endX - startX) / Mathf.Abs(endX - startX);
-        int stepY = (endY - startY) / Mathf.Abs(endY - startY);
-        int x = startX + stepX;
-        int y = startY + stepY;
-
-        while (x != endX && y != endY)
-        {
-            Piece midPiece = pieces[x, y];
-            if (midPiece != null && midPiece.team != selectedPiece.team && midPiece.rank == Rank.Pawn)
-            {
-                pieces[x, y] = null;
-                Destroy(midPiece.gameObject);
-                captured = true;
-            }
-            x += stepX;
-            y += stepY;
-        }
-
-        return captured;
-    }
-
-    private bool CapturePawnIfNeeded(int startX, int startY, int endX, int endY)
-    {
-        bool captured = Mathf.Abs(endX - startX) == 2 && Mathf.Abs(endY - startY) == 2;
-        if (captured)
-        {
-            int midX = (startX + endX) / 2;
-            int midY = (startY + endY) / 2;
-            Piece capturedPiece = pieces[midX, midY];
-            if (capturedPiece != null)
-            {
-                pieces[midX, midY] = null;
-                Destroy(capturedPiece.gameObject);
-            }
-        }
-
-        return captured;
-    }
-
-
-
-    private bool CanCapture(int x, int y, Rank rank)
-    {
-        int[] dx = { 2, 2, -2, -2 };
-        int[] dy = { 2, -2, 2, -2 };
-
-        for (int i = 0; i < 4; i++)
-        {
-            int newX = x + dx[i];
-            int newY = y + dy[i];
-
-            if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8)
-            {
-                int midX = (x + newX) / 2;
-                int midY = (y + newY) / 2;
-                Piece midPiece = pieces[midX, midY];
-
-                if (midPiece != null && midPiece.team != selectedPiece.team && pieces[newX, newY] == null)
-                {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-
-
-    private bool IsValidMove(int startX, int startY, int endX, int endY, Rank rank)
-    {
-        if (IsOutOfBounds(endX, endY) || IsOccupied(endX, endY))
-        {
-            return false;
-        }
-
-        int deltaX = Mathf.Abs(endX - startX);
-        int deltaY = Mathf.Abs(endY - startY);
-
-        bool mustCapture = MustCapture();
-
-        if (rank == Rank.Pawn)
-        {
-            return IsValidPawnMove(startX, startY, endX, endY, deltaX, deltaY, mustCapture);
-        }
-        else if (rank == Rank.Knight)
-        {
-            return IsValidKnightMove(startX, startY, endX, endY, deltaX, deltaY, mustCapture);
-        }
-
-        return false;
-    }
-
-    private bool IsOutOfBounds(int x, int y)
-    {
-        return x < 0 || x >= 8 || y < 0 || y >= 8;
-    }
-
-    private bool IsOccupied(int x, int y)
-    {
-        return pieces[x, y] != null;
-    }
-
-    private bool MustCapture()
-    {
-        for (int x = 0; x < 8; x++)
-        {
-            for (int y = 0; y < 8; y++)
-            {
-                if (pieces[x, y] != null && pieces[x, y].team == selectedPiece.team && CanCapture(x, y, pieces[x, y].rank))
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private bool IsValidPawnMove(int startX, int startY, int endX, int endY, int deltaX, int deltaY, bool mustCapture)
-    {
-        if (deltaX == 1 && deltaY == 1)
-        {
-            if ((selectedPiece.team == Team.WHITE && endY > startY) || (selectedPiece.team == Team.BLACK && endY < startY))
-            {
-                return !mustCapture;
-            }
-        }
-
-        if (deltaX == 2 && deltaY == 2)
-        {
-            int midX = (startX + endX) / 2;
-            int midY = (startY + endY) / 2;
-            Piece midPiece = pieces[midX, midY];
-            if (midPiece != null && midPiece.team != selectedPiece.team)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private bool IsValidKnightMove(int startX, int startY, int endX, int endY, int deltaX, int deltaY, bool mustCapture)
-    {
-        if (deltaX == deltaY)
-        {
-            if (mustCapture && !CanCapture(startX, startY, Rank.Knight))
-            {
-                return false;
-            }
-
-            if (deltaX == 2 && deltaY == 2)
-            {
-                int midX = (startX + endX) / 2;
-                int midY = (startY + endY) / 2;
-                Piece midPiece = pieces[midX, midY];
-                if (midPiece != null && midPiece.team != selectedPiece.team)
-                {
-                    return true;
-                }
-            }
-
-            return true;
-        }
-
-        return false;
-    }
-
-
-
-
-
-
-    private void GenerateBoard()
-    {
-        whitePiecesParent = new GameObject("WhitePieces");
-        whitePiecesParent.transform.SetParent(transform);
-        whitePiecesParent.transform.localPosition = boardOffset;
-
-        blackPiecesParent = new GameObject("BlackPieces");
-        blackPiecesParent.transform.SetParent(transform);
-        blackPiecesParent.transform.localPosition = boardOffset;
-
-        GenerateWhiteTeam();
-        GenerateBlackTeam();
-    }
-
-    private void GenerateWhiteTeam()
-    {
-        for (int x = 0; x < 8; x += 2)
-        {
-            for (int z = 0; z < 3; z++)
-            {
-                if (z % 2 == 0)
-                {
-                    GeneratePiece(x, z, Team.WHITE);
-                }
-                else
-                {
-                    GeneratePiece(x + 1, z, Team.WHITE);
-                }
-            }
-        }
-    }
-
-    private void GenerateBlackTeam()
-    {
-        for (int x = 0; x < 8; x += 2)
-        {
-            for (int z = 5; z < 8; z++)
-            {
-                if (z % 2 == 0)
-                {
-                    GeneratePiece(x, z, Team.BLACK);
-                }
-                else
-                {
-                    GeneratePiece(x + 1, z, Team.BLACK);
-                }
-            }
-        }
-    }
-
-    private void GeneratePiece(int x, int z, Team team)
-    {
-        GameObject piecePrefab = team == Team.WHITE ? whitePiecePrefab : blackPiecePrefab;
-        GameObject pieceObject = Instantiate(piecePrefab) as GameObject;
-
-        Vector3 correctedPosition = new Vector3(x, 0, z);
-        pieceObject.transform.localPosition = correctedPosition;
-        pieceObject.transform.localRotation = team == Team.WHITE ? Quaternion.Euler(-90, 0, 0) : Quaternion.Euler(-90, 180, 0);
-
-        pieceObject.transform.SetParent(team == Team.WHITE ? whitePiecesParent.transform : blackPiecesParent.transform, false);
-
-
-        Piece p = pieceObject.GetComponent<Piece>();
-        pieces[x, z] = p;
-    }
-
-    private IEnumerator RotateCamera(float duration)
-    {
-        Quaternion startRotation = Camera.main.transform.rotation;
-        Quaternion endRotation = isWhiteTurn ? startRotation * Quaternion.Euler(0, 0, 180) : startRotation * Quaternion.Euler(0, 0, -180);
-        float elapsed = 0f;
-
-        while (elapsed < duration)
-        {
-            Camera.main.transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsed / duration);
-            elapsed += Time.deltaTime;
-            yield return null;
-        }
-
-        Camera.main.transform.rotation = endRotation;
-    }
-
 }
---
+++ Assets/Scripts/PieceMovement.cs
@@ -0,0 +1,241 @@
+using UnityEngine;
+
+public static class PieceMovement
+{
+    public static void DragPiece(Piece piece)
+    {
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            piece.transform.position = hit.point + Vector3.up * 0.5f;
+        }
+    }
+
+    public static void TryMove(BoardManager boardManager, int startX, int startY, int endX, int endY, Rank rank)
+    {
+        if (IsValidMove(boardManager, startX, startY, endX, endY, rank))
+        {
+            Piece piece = boardManager.pieces[startX, startY];
+            boardManager.pieces[endX, endY] = piece;
+            boardManager.pieces[startX, startY] = null;
+            piece.transform.position = new Vector3(endX - 0.277f, 0, endY - 0.115f);
+
+            PromotePieceIfNeeded(piece, endY);
+
+            bool captured = false;
+            if (rank == Rank.Knight)
+            {
+                captured = CapturePawnsPassedByKnight(boardManager, startX, startY, endX, endY);
+            }
+            else
+            {
+                captured = CapturePawnIfNeeded(boardManager, startX, startY, endX, endY);
+            }
+
+            if (captured && CanCapture(boardManager, endX, endY, piece.rank))
+            {
+                boardManager.SelectedPiece = piece;
+                boardManager.StartDrag = new Vector2(endX, endY);
+                return;
+            }
+
+            boardManager.IsWhiteTurn = !boardManager.IsWhiteTurn;
+            boardManager.StartCoroutine(CameraController.RotateCamera(1.0f, boardManager.IsWhiteTurn));
+        }
+        else
+        {
+            boardManager.SelectedPiece.transform.position = new Vector3(startX - 0.277f, 0, startY - 0.115f);
+        }
+    }
+
+    public static void PromotePieceIfNeeded(Piece piece, int endY)
+    {
+        if (piece.team == Team.WHITE && endY == 7)
+        {
+            piece.transform.rotation = Quaternion.Euler(90, 0, 0);
+            piece.rank = Rank.Knight;
+        }
+        else if (piece.team == Team.BLACK && endY == 0)
+        {
+            piece.transform.rotation = Quaternion.Euler(90, 0, 180);
+            piece.rank = Rank.Knight;
+        }
+    }
+
+    public static bool CapturePawnsPassedByKnight(BoardManager boardManager, int startX, int startY, int endX, int endY)
+    {
+        bool captured = false;
+        int stepX = (endX - startX) / Mathf.Abs(endX - startX);
+        int stepY = (endY - startY) / Mathf.Abs(endY - startY);
+        int x = startX + stepX;
+        int y = startY + stepY;
+
+        while (x != endX && y != endY)
+        {
+            Piece midPiece = boardManager.pieces[x, y];
+            if (midPiece != null && midPiece.team != boardManager.SelectedPiece.team && midPiece.rank == Rank.Pawn)
+            {
+                boardManager.pieces[x, y] = null;
+                Object.Destroy(midPiece.gameObject);
+                captured = true;
+            }
+            x += stepX;
+            y += stepY;
+        }
+
+        return captured;
+    }
+
+
+    public static bool CapturePawnIfNeeded(BoardManager boardManager, int startX, int startY, int endX, int endY)
+    {
+        bool captured = Mathf.Abs(endX - startX) == 2 && Mathf.Abs(endY - startY) == 2;
+        if (captured)
+        {
+            int midX = (startX + endX) / 2;
+            int midY = (startY + endY) / 2;
+            Piece capturedPiece = boardManager.pieces[midX, midY];
+            if (capturedPiece != null)
+            {
+                boardManager.pieces[midX, midY] = null;
+                Object.Destroy(capturedPiece.gameObject);
+            }
+        }
+
+        return captured;
+    }
+
+    public static bool CanCapture(BoardManager boardManager, int x, int y, Rank rank)
+    {
+        int[] dx = { 2, 2, -2, -2 };
+        int[] dy = { 2, -2, 2, -2 };
+
+        for (int i = 0; i < 4; i++)
+        {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+
+            if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8)
+            {
+                int midX = (x + newX) / 2;
+                int midY = (y + newY) / 2;
+                Piece midPiece = boardManager.pieces[midX, midY];
+
+                if (midPiece != null && midPiece.team != boardManager.SelectedPiece.team && boardManager.pieces[newX, newY] == null)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    public static bool IsValidMove(BoardManager boardManager, int startX, int startY, int endX, int endY, Rank rank)
+    {
+        if (IsOutOfBounds(endX, endY) || IsOccupied(boardManager, endX, endY))
+        {
+            return false;
+        }
+
+        int deltaX = Mathf.Abs(endX - startX);
+        int deltaY = Mathf.Abs(endY - startY);
+
+        bool mustCapture = MustCapture(boardManager);
+
+        if (rank == Rank.Pawn)
+        {
+            return IsValidPawnMove(boardManager, startX, startY, endX, endY, deltaX, deltaY, mustCapture);
+        }
+        else if (rank == Rank.Knight)
+        {
+            return IsValidKnightMove(boardManager, startX, startY, endX, endY, deltaX, deltaY, mustCapture);
+        }
+
+        return false;
+    }
+
+    public static bool IsOutOfBounds(int x, int y)
+    {
+        return x < 0 || x >= 8 || y < 0 || y >= 8;
+    }
+
+    public static bool IsOccupied(BoardManager boardManager, int x, int y)
+    {
+        return boardManager.pieces[x, y] != null;
+    }
+
+    public static bool MustCapture(BoardManager boardManager)
+    {
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                if (boardManager.pieces[x, y] != null && boardManager.pieces[x, y].team == boardManager.SelectedPiece.team && CanCapture(boardManager, x, y, boardManager.pieces[x, y].rank))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static bool IsValidPawnMove(BoardManager boardManager, int startX, int startY, int endX, int endY, int deltaX, int deltaY, bool mustCapture)
+    {
+        if (deltaX == 1 && deltaY == 1)
+        {
+            if ((boardManager.SelectedPiece.team == Team.WHITE && endY > startY) || (boardManager.SelectedPiece.team == Team.BLACK && endY < startY))
+            {
+                return !mustCapture;
+            }
+        }
+
+        if (deltaX == 2 && deltaY == 2)
+        {
+            int midX = (startX + endX) / 2;
+            int midY = (startY + endY) / 2;
+            Piece midPiece = boardManager.pieces[midX, midY];
+            if (midPiece != null && midPiece.team != boardManager.SelectedPiece.team)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    public static bool IsValidKnightMove(BoardManager boardManager, int startX, int startY, int endX, int endY, int deltaX, int deltaY, bool mustCapture)
+    {
+        if (deltaX == deltaY)
+        {
+            if (mustCapture && !CanCapture(boardManager, startX, startY, Rank.Knight))
+            {
+                return false;
+            }
+
+            if (deltaX == 2 && deltaY == 2)
+            {
+                int midX = (startX + endX) / 2;
+                int midY = (startY + endY) / 2;
+                Piece midPiece = boardManager.pieces[midX, midY];
+                if (midPiece != null && midPiece.team != boardManager.SelectedPiece.team)
+                {
+                    return true;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+}
